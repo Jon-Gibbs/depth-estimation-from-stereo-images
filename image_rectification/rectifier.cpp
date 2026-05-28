6aa394d8e29046711cc06491abad6ab6e0a61c5a
Jon Gibbs <jon@gibbs.tk>
2026-05-28 10:59:52 -0700

    loadcalibration implemented

--- image_rectification/rectifier.cpp
+++ image_rectification/rectifier.cpp
@@ -14,20 +14,65 @@ StereoRectifier::StereoRectifier(const std::string &calibrationPath, cv::Size im
     {
         std::cerr << "Error: could not open file." << std::endl;
     }
-    // read each variable into the matricies
+    // inititalize intrinsics matricies
     K_L_ = cv::Mat::zeros(3, 3, CV_32F);
     K_R_ = cv::Mat::zeros(3, 3, CV_32F);
-    K_L_[0][0] = config["left_camera"]["fx"] this->imageSize_ = imageSize;
+    // intialize distortion matricies
+    dist_L_ = cv::Mat<cv::Mat<float>>::zeroes(1, 5, CV_32F);
+    dist_R_ = cv::Mat<cv::Mat<float>>::zeroes(1, 5, CV_32F);
+    // initaliize transaltion and rotation
+    R_ = cv::Mat<cv::Mat<float>>::zeroes(1, 2, CV_32F);
+    T_ = cv::Mat<cv::Mat<float>>::zeroes(1, 2, CV_32F);
+    //
+    // populate matricies
+    loadCalibration(calibrationPath);
+    this->imageSize_ = imageSize;
 }
 // PRE:  path points to a valid YAML file containing all required calibration fields
 // POST: K_L_, dist_L_, K_R_, dist_R_, R_, T_ are populated
 void StereoRectifier::loadCalibration(const std::string &path)
 {
+    // populate intrinsics matricies
+    // left camera
+    K_L_[0][0] = config["left_camera"]["intrinsics"]["fx"].as<float>();
+    K_L_[1][1] = config["left_camera"]["intrinsics"]["fy"].as<float>();
+    K_L_[0][2] = config["left_camera"]["intrinsics"]["cx"].as<float>();
+    K_L_[1][2] = config["left_camera"]["intrinsics"]["fy"].as<float>();
+    K_L_[2][2] = 1;
+    // right camera
+    K_R_[0][0] = config["right_camera"]["intrinsics"]["fx"].as<float>();
+    K_R_[1][1] = config["right_camera"]["intrinsics"]["fy"].as<float>();
+    K_R_[0][2] = config["right_camera"]["intrinsics"]["cx"].as<float>();
+    K_R_[1][2] = config["right_camera"]["intrinsics"]["fy"].as<float>();
+    K_R_[2][2] = 1;
+    // populate distortion matricies
+    // left camera
+    dist_L_[0][0] = config["left_camera"]["disto"]["k1"].as<float>();
+    dist_L_[0][1] = config["left_camera"]["disto"]["k2"].as<float>();
+    dist_L_[0][2] = config["left_camera"]["disto"]["p1"].as<float>();
+    dist_L_[0][3] = config["left_camera"]["disto"]["p2"].as<float>();
+    dist_L_[0][4] = config["left_camera"]["disto"]["k3"].as<float>();
+    // right camera
+    dist_R_[0][0] = config["left_camera"]["disto"]["k1"].as<float>();
+    dist_R_[0][1] = config["left_camera"]["disto"]["k2"].as<float>();
+    dist_R_[0][2] = config["left_camera"]["disto"]["p1"].as<float>();
+    dist_R_[0][3] = config["left_camera"]["disto"]["p2"].as<float>();
+    dist_R_[0][4] = config["left_camera"]["disto"]["k3"].as<float>();
+    // populate rotation and tranlsation matricies
+    // rotation
+    R_[0][0] = config["translation"]["x"].as<float>();
+    R_[0][1] = config["translation"]["y"].as<float>();
+    R_[0][2] = config["translation"]["z"].as<float>();
+    // translation
+    T_[0][0] = config["translation"]["x"].as<float>();
+    T_[0][1] = config["translation"]["y"].as<float>();
+    T_[0][2] = config["translation"]["z"].as<float>();
 }
 // PRE:  K_L_, dist_L_, K_R_, dist_R_, R_, T_, imageSize_ are all populated
 // POST: R_L_, R_R_, P_L_, P_R_, Q_ are populated
 void StereoRectifier::computeRectification()
 {
+    // make a
 }
 // PRE:  R_L_, R_R_, P_L_, P_R_, K_L_, K_R_, dist_L_, dist_R_, imageSize_ are all populated
 // POST: map_L1_, map_L2_, map_R1_, map_R2_ are populated and ready for use
